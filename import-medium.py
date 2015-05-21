b671b9d7f2e6aaa6eaf503985d3df5bc4495c399
Mark Biek <mark.biek@kindred.com>
2015-05-21 10:23:39 -0400

    Basic parsing is working. Next is to import from url

--- import-medium.py
+++ import-medium.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python
 
+import json
+
+def cleanText(text):
+    patterns = ['s','m','d','ve','ll','t','re']
+    for p in patterns:
+        text = text.replace('.'+p, "'"+p)
+
+    return text
+
+raw_json = ''.join(open('medium-post.json', 'r').readlines())
+json = json.loads(raw_json)
+
+content = json['embedded']['value']['content']
+paragraphs = content['bodyModel']['paragraphs']
+
+for p in paragraphs:
+    if p['text'] != "":
+        text = cleanText(p['text'])
+
+        if p['type'] == 3:
+            text = "### " + text
+
+        print(text + "\n")

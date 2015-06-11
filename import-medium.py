41cf176e3ff596fae22595ef22955c0ea9cc24f5
Mark Biek <mark.biek@kindred.com>
2015-06-11 10:01:37 -0400

    First pass at retrieving post json from a url

--- import-medium.py
+++ import-medium.py
@@ -1,6 +1,32 @@
 #!/usr/bin/python
 
+import sys
 import json
+import urllib
+from HTMLParser import HTMLParser
+
+class MediumHtmlParser(HTMLParser):
+    collect_data = False
+    raw_json = ""
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "script":
+            self.collect_data = True
+
+    def handle_endtag(self, tag):
+        if tag == "script" and self.collect_data:
+            self.collect_data = False
+
+    def handle_data(self, data):
+        if self.collect_data:
+            if "<![CDATA[" in data and "var GLOBALS" in data:
+                data = data.replace("// <![CDATA[", "")
+                data = data.replace("var GLOBALS = ", "")
+                data = data.replace("// ]]>", "")
+
+                print(data)
+
+"""
 
 def cleanText(text):
     patterns = ['s','m','d','ve','ll','t','re']
@@ -23,3 +49,18 @@ for p in paragraphs:
             text = "### " + text
 
         print(text + "\n")
+"""
+
+def usage():
+    print("Usage: import-medium.py <url>")
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        usage()
+        sys.exit(1)
+
+    url = sys.argv[1]
+    f = urllib.urlopen(url)
+
+    parser = MediumHtmlParser()
+    parser.feed(f.read())
